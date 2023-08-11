b004e7f380768801138c51f1540e33fbe54b609d
Hacker <Hacker@MacBook-Pro-2.local>
2023-08-11 15:43:57 +0600

    Creating a URL model and applying migrations

--- shortener/models.py
+++ shortener/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class URL(models.Model):
+    original_url = models.URLField()
+    shortened_url = models.CharField(max_length=15, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
